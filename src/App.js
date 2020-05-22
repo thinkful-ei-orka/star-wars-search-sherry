334fc35bce6f52a7c667c5a05607891a993d04b1
Sherry McCarty <sherrydmccarty@gmail.com>
2020-05-22 11:20:42 -0400

    Added Header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
+import Header from './components/Header'
+
 function App() {
   return (
     <main className='App'>
-      {/* content goes here */}
+      <Header />
     </main>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './Header.css';
+
+function Header() {
+  return (
+    <header>
+      <h1>Star Wars Search</h1>
+    </header>
+  )
+}
+
+export default Header
